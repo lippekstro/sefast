d1d110d55cd25620503ff605dd82d7539ddece4d
lippekstro <fellipecastro13@hotmail.com>
2024-05-12 00:05:30 -0300

    pdo com utf8

--- db/conexao.php
+++ db/conexao.php
@@ -5,7 +5,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/sefast/configs/config.php';
 class Conexao{
 
     public static function criarConexao(){
-        $conn = new PDO(SGBD . ':host=' . LOCALDOBANCO . ';dbname=' . NOMEDOBANCO, USUARIO, SENHA);
+        $conn = new PDO(SGBD . ':host=' . LOCALDOBANCO . ';dbname=' . NOMEDOBANCO . ";charset=utf8", USUARIO, SENHA);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
     }
